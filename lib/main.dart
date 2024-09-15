c080096c6130a05f2be369a99c83cfb49f6ea0e0
Hira0523 <hiranosya0523.uw@gmail.com>
2024-09-15 18:29:27 +0900

    ElevatedButtonウィジェット実装

--- lib/main.dart
+++ lib/main.dart
@@ -34,31 +34,16 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: TextButton(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: ElevatedButton(
               onPressed: () {
-                // ここに押した時の処理を書きます。
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('お知らせ'),
-                        content: const Text('アラートダイアログです！'),
-                        actions: [
-                          TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('Close'))
-                        ],
-                      );
-                    });
+                // ここに押した時の処理を記述します。
               },
-              child: const Text('押してね'))),
-    );
+              child: const Text('押してね')),
+        ));
   }
 }
